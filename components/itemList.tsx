c7d296fcb98a5b39c21eaa4d2259af2721be1532
Takano Ryo <takano@096-118097-04.local>
2020-11-09 13:51:17 +0900

    検索窓を追加

---
+++ components/itemList.tsx
@@ -0,0 +1,100 @@
+import { Item } from "./Items";
+
+export const items = [
+  {
+    name: "2t ダンプ",
+    image: require("../assets/0f62134bf3e8ef07bc0f4f43076a3204.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "2t トラッククレーン付き",
+    image: require("../assets/1afd850e68d4297b622ab28d01a4b2ac.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "ワンボックス",
+    image: require("../assets/2b45c503eec0ec1bd12c27d911b2e230.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "parts a",
+    image: require("../assets/8ce94c3bf2b79dbf5c771da11e08941f.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "parts b",
+    image: require("../assets/22b080faae9bb2bf646ee92ef42b2b4c.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane c",
+    image: require("../assets/75fca98d5ab45ffc1e5d02f4e25435c0.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane d",
+    image: require("../assets/77cf4f40da07636d792d5b9246648232.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane e",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane e",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane g",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane h",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane i",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane j",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: 150000,
+  },
+  {
+    name: "crane k",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: "150000",
+  },
+  {
+    name: "crane l",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: "150000",
+  },
+  {
+    name: "crane ",
+    image: require("../assets/338cabceba0d935585e0dfdf8ff6ed77.jpg"),
+    category: "掘削・運搬・解体・林業",
+    price: "150000",
+  },
+] as Item[];
--- screens/profile/ProfileScreen.tsx
+++ screens/profile/ProfileScreen.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { Divider, Layout, Text } from "@ui-kitten/components";
+import { Divider, Input, Layout, Text } from "@ui-kitten/components";
 import { ProfileScreenProps } from "../../navigation/profile.navigator";
 import { Toolbar } from "../../components/Toolbar";
 import {
@@ -8,39 +8,60 @@ import {
   SafeAreaLayoutElement,
   SaveAreaInset,
 } from "../../components/SafeAreaLayout";
-import { MenuIcon } from "../../assets/icons";
+import { MenuIcon, SearchIcon } from "../../assets/icons";
 import { APP_NAME } from "../../constants";
 import { Categories } from "../../components/Categories";
-import { Items } from "../../components/Items";
+import { Item, Items } from "../../components/Items";
+import { items as allItems } from "../../components/itemList";
 
 export const ProfileScreen = (
   props: ProfileScreenProps
-): SafeAreaLayoutElement => (
-  <SafeAreaLayout style={styles.safeArea} insets={SaveAreaInset.TOP}>
-    <Toolbar
-      title={APP_NAME}
-      backIcon={MenuIcon}
-      onBackPress={props.navigation.toggleDrawer}
-    />
-    <Divider />
-    <View style={styles.text}>
-      <Text>Category</Text>
-    </View>
-    <Layout>
-      <View>
-        <Categories />
+): SafeAreaLayoutElement => {
+  const [query, setQuery] = React.useState<string>("");
+  const [items, setItems] = React.useState<Item[]>(allItems);
+
+  const onChangeQuery = (query: string): void => {
+    // const nextTodos: Todo[] = allTodos.filter((todo: Todo): boolean => {
+    //   return todo.title.toLowerCase().includes(query.toLowerCase());
+    // });
+    // setTodos(nextTodos);
+    // setQuery(query);
+  };
+
+  return (
+    <SafeAreaLayout style={styles.safeArea} insets={SaveAreaInset.TOP}>
+      <Toolbar
+        title={APP_NAME}
+        backIcon={MenuIcon}
+        onBackPress={props.navigation.toggleDrawer}
+      />
+      <Divider />
+      <Input
+        style={styles.filterInput}
+        placeholder="Search"
+        value={query}
+        accessoryLeft={SearchIcon}
+        onChangeText={onChangeQuery}
+      />
+      <View style={styles.text}>
+        <Text>Category</Text>
       </View>
-    </Layout>
-    <View style={styles.text}>
-      <Text>Items</Text>
-    </View>
-    <Layout>
-      <View>
-        <Items />
+      <Layout>
+        <View>
+          <Categories />
+        </View>
+      </Layout>
+      <View style={styles.text}>
+        <Text>Items</Text>
       </View>
-    </Layout>
-  </SafeAreaLayout>
-);
+      <Layout>
+        <View>
+          <Items items={items} />
+        </View>
+      </Layout>
+    </SafeAreaLayout>
+  );
+};
 
 const styles = StyleSheet.create({
   safeArea: {
@@ -65,5 +86,9 @@ const styles = StyleSheet.create({
   },
   avatar: {
     margin: 8,
+  },
+  filterInput: {
+    marginTop: 16,
+    marginHorizontal: 8,
   },
 });
